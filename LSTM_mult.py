55b8b0f70171075c6162e720bc986f772af15038
stoneshaker <stoneshaker73@gmail.com>
2025-07-21 22:24:16 -0400

    Added multivariate example

---
+++ LSTM_mult.py
@@ -0,0 +1,114 @@
+import datetime
+import sklearn
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.decomposition import KernelPCA
+import numpy as np
+import pandas as pd
+import math
+import keras
+import matplotlib.pyplot as plt
+import tensorflow as tf
+tf.random.set_seed(99)
+
+# Dataset loading
+dataFrame = pd.read_csv('.\\data\\final_data_adj.csv')  # https://github.com/SusmitSekharBhakta/Stock-market-price-prediction/blob/main/final_data_adj.csv
+
+imputer = SimpleImputer(missing_values=np.nan)  # Handling missing values
+dataFrame.drop(columns=['Date'], inplace=True)
+dataFrame = pd.DataFrame(imputer.fit_transform(dataFrame), columns=dataFrame.columns)
+dataFrame = dataFrame.reset_index(drop=True)
+# Applying feature scaling
+scaler = MinMaxScaler(feature_range=(0, 1))
+df_scaled = scaler.fit_transform(dataFrame.to_numpy())
+df_scaled = pd.DataFrame(df_scaled, columns=list(dataFrame.columns))
+target_scaler = MinMaxScaler(feature_range=(0, 1))
+df_scaled[['Open', 'Close']] = target_scaler.fit_transform(dataFrame[['Open', 'Close']].to_numpy())
+df_scaled = df_scaled.astype(float)
+
+# Single step dataset preparation
+def singleStepSampler(df, window):
+    xRes = []
+    yRes = []
+    for i in range(0, len(df) - window):
+        res = []
+        for j in range(0, window):
+            r = []
+            for col in df.columns:
+                r.append(df[col][i + j])
+            res.append(r)
+        xRes.append(res)
+        yRes.append(df[['Open', 'Close']].iloc[i + window].values)
+    return np.array(xRes), np.array(yRes)
+
+    # Dataset splitting
+SPLIT = 0.85
+(xVal, yVal) = singleStepSampler(df_scaled, 20)
+X_train = xVal[:int(SPLIT * len(xVal))]
+y_train = yVal[:int(SPLIT * len(yVal))]
+X_test = xVal[int(SPLIT * len(xVal)):]
+y_test = yVal[int(SPLIT * len(yVal)):]
+print("X_train shape =",X_train.shape)
+print("y_train shape =",y_train.shape)
+
+multivariate_lstm = keras.Sequential()
+multivariate_lstm.add(keras.layers.Input(shape=(X_train.shape[1], X_train.shape[2])))
+multivariate_lstm.add(keras.layers.LSTM(200)) #, input_shape=(X_train.shape[1], X_train.shape[2])))
+multivariate_lstm.add(keras.layers.Dropout(0.2))
+multivariate_lstm.add(keras.layers.Dense(2, activation='linear'))
+multivariate_lstm.compile(loss = 'MeanSquaredError', metrics=['MAE'], optimizer='Adam')
+multivariate_lstm.summary()
+
+history = multivariate_lstm.fit(X_train, y_train, epochs=200)
+
+# Reload the data with the date index
+dataFrame = pd.read_csv('.\\data\\final_data_adj.csv')  # Assuming the CSV file contains a 'Date' column
+dataFrame['Date'] = pd.to_datetime(dataFrame['Date'])
+dataFrame.set_index('Date', inplace=True)
+
+# Forecast Plot with Dates on X-axis
+predicted_values = multivariate_lstm.predict(X_test)
+
+d = {
+    'Predicted_Open': predicted_values[:, 0],
+    'Predicted_Close': predicted_values[:, 1],
+    'Actual_Open': y_test[:, 0],
+    'Actual_Close': y_test[:, 1],
+}
+
+d = pd.DataFrame(d)
+d.index = dataFrame.index[-len(y_test):]  # Assigning the correct date index
+
+fig, ax = plt.subplots(figsize=(10, 6))
+#  highlight the  forecast
+highlight_start = int(len(d) * 0.9)  
+highlight_end = len(d) - 1  # Adjusted to stay within bounds
+# Plot the actual values
+plt.plot(d[['Actual_Open', 'Actual_Close']][:highlight_start], label=['Actual_Open', 'Actual_Close'])
+
+# Plot predicted values with a dashed line
+plt.plot(d[['Predicted_Open', 'Predicted_Close']], label=['Predicted_Open', 'Predicted_Close'], linestyle='--')
+
+# Highlight the forecasted portion with a different color
+plt.axvspan(d.index[highlight_start], d.index[highlight_end], facecolor='lightgreen', alpha=0.5, label='Forecast')
+
+plt.title('Multivariate Time-Series forecasting using LSTM')
+plt.xlabel('Dates')
+plt.ylabel('Values')
+ax.legend()
+plt.show()
+
+# Model Evaluation
+def eval(model):
+    return {
+        'MSE': sklearn.metrics.mean_squared_error(d[f'Actual_{model.split("_")[1]}'].to_numpy(), d[model].to_numpy()),
+        'MAE': sklearn.metrics.mean_absolute_error(d[f'Actual_{model.split("_")[1]}'].to_numpy(), d[model].to_numpy()),
+        'R2': sklearn.metrics.r2_score(d[f'Actual_{model.split("_")[1]}'].to_numpy(), d[model].to_numpy())
+    }
+
+result = dict()
+
+for item in ['Predicted_Open', 'Predicted_Close']:
+    result[item] = eval(item)
+
+print(result)
